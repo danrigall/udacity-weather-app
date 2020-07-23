b631cf94ed048e13c9e908b2ed83ac68c28dc8d3
Dan Rigall <danrigall@gmail.com>
2020-07-22 22:23:27 -0700

    Fixed some logic bugs. UI now updates correctly.

--- website/app.js
+++ website/app.js
@@ -3,18 +3,44 @@ let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
 // TODO: FIGURE OUT CLICK EVENT!!!!!!!!!!
-document.getElementById('submit').addEventListener('click', clickRespond)
+function checkEmpty() {
+  const zip = document.getElementById('zip').value
+  const feelings = document.getElementById('feelings').value
+  if (zip === "" || feelings === "") {
+    console.log('zip and/or feelings are empty');
+  } else {
+    clickRespond()
+  }
+}
 
 function clickRespond(evt) {
-  const thoughts = document.getElementById('thoughts').value;
+  const thoughts = document.getElementById('feelings').value;
   // Dummy API call
   getTemp('/fakeAPI')
   .then(function(temp) {
     console.log(temp)
     postData('/add', {temp, date: newDate, thoughts: thoughts})
-    updateUI()
+  })
+  .then(function() {
+    updateUI();
   })
 }
+
+// function performAction(e) {
+//   const zip = document.getElementById('zip').value;
+//   const feelings = document.getElementById('feelings').value;
+//   getWeather(baseURL, zip, apiKey)
+//     .then(function (data) {
+//       console.log(data);
+//       postData('/addData', {
+//         date: d,
+//         temp: data.main.temp,
+//         content: feelings
+//       })
+//     })
+//     .then(function () { updateUI() }
+//     )
+// }
 
 // Global Variables
 const baseURL = 'https://openweathermap.org/current';
@@ -56,10 +82,15 @@ const updateUI = async()=> {
   try{
     const allData = await request.json()
     console.log(allData)
+    document.getElementById('entryHolder').style.display = 'block'
     document.getElementById('date').innerHTML = allData[0].date
     document.getElementById('temp').innerHTML = allData[0].temp
-    document.getElementById('posted-thoughts').innerHTML = allData[0].thoughts
+    document.getElementById('content').innerHTML = allData[0].thoughts
   } catch(error){
     console.log('ERROR in UI update:', error)
   }
 }
+
+document.getElementById('generate').addEventListener('click', checkEmpty)
+
+// checkEmpty();
